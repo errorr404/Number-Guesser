8cd266d46abc0f8ebd3575e088a338b80d9b9ef2
errorr404 <dkbishwash@gmail.com>
2018-05-11 16:38:22 +0530

    UI design and Validation

---
+++ app.js
@@ -0,0 +1,88 @@
+/*
+GAME FUNCTION:
+-Player must guess a number between a min and max
+-Player gets a certain amount of guesses
+-Notify player of gusses remaining
+-Notify the player of the correct answer if looses
+-Let player choose to play again
+*/
+/* // My code :) :) :) :) :)
+// define the event of submit button
+document.getElementById('guess-value').addEventListener('click',checkResult);
+
+
+
+
+// define the checkResult function
+function checkResult(e){
+  const guessNumber = parseInt(document.querySelector('#guess-input').value);
+  
+  if(count>1 && guessNumber===5){
+    alert('great...')
+  }
+  else if(count>1){
+    count= count-1;
+  }
+  else
+  {
+    document.getElementById('guess-input').style.display= 'btn-danger';
+  }
+
+  e.preventDefault();
+}
+
+
+
+// select a guess number
+
+const randGuess = Math.floor(Math.random()*10+1);
+let count =3;
+
+*/
+
+
+// game values
+let min = 1, max=10;
+let winnigNum = Math.floor(Math.random()*(max-min+1)+min);
+let guessleft = 3;
+
+// UI element
+const game = document.getElementById('game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// create an event listner
+
+guessBtn.addEventListener('click',function(){
+let guess = parseInt(guessInput.value);
+//validate
+if(isNaN(guess) || guess< min|| guess> max){
+  setMessage('Please enter a number between ${min} and ${max}', 'red')
+}
+// check if won
+if(guess === winnigNum){
+// disable the inout
+guessInput.disabled = true;
+// change the border color
+guessInput.style.borderColor = 'green';
+//set messege
+setMessage('${winnigNum} is correct,you Win!','green');
+}
+else{
+
+}
+
+});
+
+// set messege
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
